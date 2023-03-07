89090b0d197f02646bb8f54ff4210670dee8a1c8
Ronald Suplina <tyger995@gmail.com>
2023-03-07 21:41:38 +0000

    Builds UI FIX

--- aoe4-builder-order/src/pages/submit-build.tsx
+++ aoe4-builder-order/src/pages/submit-build.tsx
@@ -46,10 +46,12 @@ const SubmitPage: NextPage = () => {
             <option value="Mongols">Mongols</option>
             <option value="Delhi Sultanate">Delhi Sultanate</option>
             <option value="Abbasid Dynasty">Abbasid Dynasty</option>
+            <option value="Ottomans">Ottomans</option>
           </select>
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
 
-          <textarea value={build} onChange={(e) => setBuildOrder(e.target.value)} required  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
+          <textarea value={build} onChange={(e) => setBuildOrder(e.target.value)} required  
+          className="block p-2.5 w-[500px] h-[200px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" ></textarea>
           <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit Build</button>
         </form>
       
