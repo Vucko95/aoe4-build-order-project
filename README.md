091313f9ea562640f3b2904b2cae15f62e78d0ca
Ronald Suplina <tyger995@gmail.com>
2023-03-08 22:51:37 +0000

    Latest Updates

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # AOE4 Build Order Website Project
 
 [![Stack](https://skills.thijs.gg/icons?i=nextjs,ts,prisma,postgres,tailwind&theme=dark)](https://skills.thijs.gg)
-### TODO:
+### TODO: :underage:
 
-CURRENT: 
+CURRENT:  :clock130:
 
 - As a User I wanna Create a Specific Build which will have a type and short Description
-    - ADD ABILITY TO ADD 
-        - Build Type (Fast Castle, etc...)
-        - Description  (This build is for ....)
-    - Add Ability to Remove Certian Build
-    - Create Page to Display Full Build
-        - Display a Build On new Page once User Click on display Build
-    - Enhance Build Creation
-    - Enhance Build Display Page
+    - ADD ABILITY TO ADD :heavy_check_mark:
+        - Build Type (Fast Castle, etc...) :heavy_check_mark:
+        - Description  (This build is for ....) :heavy_check_mark:
+    - Add Feature to Remove Certian Build  :heavy_exclamation_mark:
+- As a User I want check out each build
+    - Create Modal to Display Full Build
+        - Display a Full Build once user clicks on display build
+- As A User I want to Filter builds based on Civilization 
+- ADDITIONALS 
+    - Enhance Submit Build Display Page
+        - On a side Add a Demo Build Example 
+        - Add "Asset" Icons for base game Assets
--- aoe4-builder-order/src/pages/builds.tsx
+++ aoe4-builder-order/src/pages/builds.tsx
@@ -19,9 +19,9 @@ const BuildsPage: NextPage = () => {
       </h1>
     <div className="grid grid-cols-4 gap-4 grid-flow-row w-[60%] ml-[20%] pt-20">
       {builds.data?.map((build) => (
-        <div className="flex flex-col justify-center items-center w-[250px] h-[300px] p-2.5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div key={build.id} className="flex flex-col justify-center items-center w-[250px] h-[300px] p-2.5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a  href="#">
-            <img className="  h-[150px]" m-l-10 src={`/aoe4/${build.civilization.toLowerCase()}.png`} alt="" />
+            <img className="  h-[150px]"  src={`/aoe4/${build.civilization.toLowerCase()}.png`} alt="" />
         </a>
             <div className="p-5">
                 <a href="#">
@@ -29,18 +29,60 @@ const BuildsPage: NextPage = () => {
                     {build.civilization}
                         </h5>
                 </a>
+                <a href="#">
+                    <p className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {build.desc}
+                        </p>
+                </a>
                     <div className=" items-center w-[200px] h-[40px]">
 
                     <a href="#" className="inline-flex items-center  px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Check Build
-                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
+                    {/* <button data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+  Toggle modal
+</button> */}
                     </div>
             </div>
         </div>
            ))  
         }
     </div>
+
+    {/* <div id="defaultModal" tabIndex="-1" aria-hidden="true" className="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+    <div className="relative w-full h-full max-w-2xl md:h-auto">
+      
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+     
+            <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    Terms of Service
+                </h3>
+                <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
+                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    <span className="sr-only">Close modal</span>
+                </button>
+            </div>
+        
+            <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
+                </p>
+            </div>
+         
+            <div className="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                <button data-modal-hide="defaultModal" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
+                <button data-modal-hide="defaultModal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
+            </div>
+        </div>
+    </div>
+</div> */}
+
+
         {/* TABLE */}
 {/* <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
