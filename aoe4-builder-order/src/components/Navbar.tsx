fc663f3405b691dc7f86a00096040dd95315a9dc
Ronald Suplina <tyger995@gmail.com>
2023-03-16 21:46:09 +0000

    Cat

--- aoe4-builder-order/src/components/Navbar.tsx
+++ aoe4-builder-order/src/components/Navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
         <img src="/aoe4/giphy4.gif" className=" mr-3 mb-4 h-12" alt="Flowbite Logo" />
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AOE 4 Build Order</span>
     </a>
-    <img src="/aoe4/cat.gif" className="    absolute top-3  left-1/2 h-1/6" alt="Flowbite Logo" />
+    <img src="/aoe4/cat.gif" className="    absolute top-0  left-1/3 h-1/6" alt="Flowbite Logo" />
 
     <button data-collapse-toggle="navbar-dropdown" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
