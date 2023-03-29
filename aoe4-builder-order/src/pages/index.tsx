2edcfda9f71adb26a96cedbfb99855fd4bdc4991
Ronald Suplina <tyger995@gmail.com>
2023-03-29 20:05:54 +0100

    Img Resize

--- aoe4-builder-order/src/pages/index.tsx
+++ aoe4-builder-order/src/pages/index.tsx
@@ -30,7 +30,7 @@ const HomePage: NextPage = () => {
     
 
 
-    <img  src="/aoe4/jumpdown.gif" className="object-cover w-full rounded-t-lg  md:h-auto md:w-80 md:rounded-none md:rounded-l-lg"  alt=""/> 
+    <img  src="/aoe4/jumpdown.gif" className="object-cover w-full rounded-t-lg  md:h-auto md:w-40 md:rounded-none md:rounded-l-lg"  alt=""/> 
         {/* <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
     <img  src="warrior.svg" className="object-cover w-full rounded-t-lg h-60 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"  alt=""/> 
     <div className="flex flex-col justify-between p-4 leading-normal">
